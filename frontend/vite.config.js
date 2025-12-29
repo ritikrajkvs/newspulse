4e39930b3dad0ec0a7ef2295a090c53d8ebbd4ef
ritikrajkvs <140308348+ritikrajkvs@users.noreply.github.com>
2025-12-29 22:37:54 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // Points to backend in the same container
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
